1d8c029a4ce86e37daa654c87779662e486fbe78
fariez11 <fariezilham03@gmail.com>
2022-08-29 15:56:22 +0700

    latihan soal

---
+++ materi18 (perulangan).js
@@ -0,0 +1,38 @@
+// control flow ------------------------------
+var angka = prompt('masukkan angka :');
+
+if(angka % 2 === 0){
+    alert(angka + 'adalah bilangan GENAP');
+}else{
+    alert(angka + 'adalah bilangan GANJIL');
+}
+
+// while -------------------------------------
+// nilai awal                                 // Rumus ----
+// while(kondisi terminasi){
+//     aksi
+
+//     increment / decrement
+// }
+
+var ulang = true;
+while(ulang){   
+    console.log('hello world');
+    ulang = confirm('lagi?');
+}
+
+var nilaiAwal = 1;
+while(nilaiAwal <= 5){
+    console.log('Hello World! ' + nilaiAwal + 'x');
+    nilaiAwal++ ;
+}
+
+// latihan ------------------------------------------
+
+var angkot = 1;
+
+while(angkot <=10){
+    console.log('Angkot No. ' + angkot + ' beroprasi dengan baik.');
+    angkot++ ;
+}
+
