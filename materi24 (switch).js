56d6316db5fc79d1b212862f171c6c64b8bcb78d
fariez11 <fariezilham03@gmail.com>
2022-08-31 14:13:47 +0700

    add file catatan materi

---
+++ materi24 (switch).js
@@ -0,0 +1,21 @@
+// switch case -----------------------------------------------
+// switch(ekpresi){                            //Rumus--------
+//     case "nilai 1":
+//         aksi 1
+//     [break;]
+//     case "nilai 2":
+//         aksi 2
+//     [break;]
+//     case "nilai n":
+//         aksi n
+//     [break;]
+//     default:
+//         aksi default
+//     [break;]
+// }
+
+/* catatan 24 ----------------------------------------------- */
+
+
+// contoh ----------------------------------------------------
+ 
