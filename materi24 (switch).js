63981464c3a0846eefd5c3e575940d761bfb2d1d
fariez11 <fariezilham03@gmail.com>
2022-09-05 14:30:41 +0700

    materi 24(switch)

--- materi24 (switch).js
+++ materi24 (switch).js
@@ -14,8 +14,42 @@
 //     [break;]
 // }
 
-/* catatan 24 ----------------------------------------------- */
-
+// var angka = parseInt(prompt('masukkan angka :'));
+// *catatan 24
 
 // contoh ----------------------------------------------------
- 
+// var angka = prompt('masukkan angka :');
+
+// switch(angka){
+//     case '1':
+//         alert('anda memasukkan angka 1');
+//         break;11
+//     case '2':
+//         alert('anda memasukkan angka 2');
+//         break;
+//     case '3':
+//         alert('anda memasukkan angka 3');
+//         break;
+//     default:
+//         alert('angka yang anda masukkan salah');
+//         break;
+// }
+
+// contoh 2----------------------------------------------------
+
+var item = prompt('masukkan nama makanan / minuman : \n (contoh: nasi, daging, susu, hamburger, sofdrink)');
+
+switch( item ){
+    case 'nasi':
+    case 'daging':
+    case 'susu':
+        alert('makanan / minuman SEHAT!');
+        break;
+    case 'hamburger':
+    case 'softdrink':
+        alert('makanan / minuman TIDAK SEHAT!');
+        break;
+    default :
+        alert('anda memasukkan nama makanan/minuman yang salah');
+        break;  
+}
