994b7f5e2b7c1550d7b7300a518b2aa4aae391b3
fariez11 <fariezilham03@gmail.com>
2022-09-05 17:11:01 +0700

    materi26

--- materi26.js
+++ materi26.js
@@ -1,2 +1,46 @@
 // membuat game suit jawa ----------------------------------------------------
+var terus = true;
+while( terus ){
+    // 1. menangkap pilihan player
+    var player = prompt('pilih : gajah, semut, orang');
+
+    // 2. menangkap pilihan computer
+    // 3. membengkitkan biangan random
+    var comp = Math.random();
+    if(comp < 0.34){
+        comp = 'gajah';
+    } else if(comp >= 0.34 && comp < 0.67){
+        comp = 'orang';
+    }else{
+        comp = 'semut';
+    }
+    var hasil = '';
+
+    // 4. menentukan rules
+    if(player == comp){
+        hasil = 'SERI!';
+    }else if(player == 'gajah'){
+        // if(comp == 'orang'){
+        //     hasil = 'MENANG!';
+        // }else{
+        //     hasil = 'KALAH!';
+        // }
+
+        hasil = (comp == 'orang') ? 'MENANG!' : 'KALAH!';               // ternary
+    }else if(player == 'orang'){
+        hasil = (comp == 'gajah') ? 'KALAH!' : 'MENANG!';
+    }else if(player == 'semut'){
+        hasil = (comp == 'orang') ? 'KALAH!' : 'MENANG!';
+    }else{
+        hasil = 'Anda memasukkan pilihan yang salah !';
+    }
+
+    // 5. tampilkan hasilnya
+
+    alert('kamu memilih ' + player + ' dan komputer memilih ' + comp + '\nyang menghasilkan anda ' + hasil);
+
+    terus = confirm('masih mau main lagi?');
+}
+
+alert('wahh kamu noob sekali ya, jadi males main sama kamu!!!');
 
