26af5d574d4ef28a4b86ad02d2c65ef73d5cd6cb
fariez11 <fariezilham03@gmail.com>
2022-09-07 14:06:06 +0700

    belajar pengulangan dan pengkondisian bersarang

--- materi25.js
+++ materi25.js
@@ -1,22 +1,23 @@
 // pengeulangan&pengkondisian bersarang -----------------------------------------
 // contoh -----------------------------------------------------------------------
-var s = '';
+    // var s = '';
 
-// for(var i = 0; i < 5; i++){
-//     for(var j = 0; j <= i; j++){ 
-//         s += '*';
-//     }
-//     s += '\n';
-// }
-// console.log(s);
+    // for(var i = 0; i < 5; i++){
+    //     for(var j = 0; j <= i; j++){ 
+    //         s += '*';
+    //     }
+    //     s += '\n';
+    // }
+    // console.log(s);
 
 // contoh 2 ---------------------------------------------------------------------
-for(var i = 5; i > 0; i--){
-    for(var j = 0; j < i; j++){ 
-        s += '*';
+    var s = '';
+    for(var i = 5; i > 0; i--){
+        for(var j = 0; j < i; j++){ 
+            s += '*';
+        }
+        s += '\n';
     }
-    s += '\n';
-}
-console.log(s);
+    console.log(s);
 
 // catatan25
