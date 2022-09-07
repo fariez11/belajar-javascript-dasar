4688ed0ae1ea8cbc7856199f3441ddd2961335b0
fariez11 <fariezilham03@gmail.com>
2022-09-07 12:00:02 +0700

    materi 29 + catatan

---
+++ materi28-29.js
@@ -0,0 +1,67 @@
+// menghitung luas kubus ----------------------------------------
+
+    // var a = 8;
+    // var b = 3;
+    // var volumeA;
+    // var volumeB;
+    // var total;
+
+    // volumeA = a * a * a;
+    // volumeB = b * b * b;
+
+
+    // total = volumeA + volumeB;
+
+    // console.log(total);
+
+// menggunakan function ------------------------------------------
+    // function jumlahVolumeDuaKubus(a,b){
+    //     var volumeA, volumeB, total;
+
+    //     volumeA = a * a * a;
+    //     volumeB = b * b * b;
+
+    //     total = volumeA + volumeB;
+
+    //     return total;
+    // }
+
+    // alert(jumlahVolumeDuaKubus(8,3));
+    // alert(jumlahVolumeDuaKubus(10,12));
+
+// parameter & argument --------------------------------------------
+
+    // function tambah(a,b){
+    //     return a + b;
+    // }
+
+    // var a = parseInt(prompt('Masukkan nilai 1;'));
+    // var b = parseInt(prompt('Masukkan nilai 2;'));
+
+    // // var coba = tambah(a*2,b*2);          // ini juga bisa
+    // var coba = tambah(a,b);
+    // console.log(coba);
+
+// contoh2 ----------------------------------------------------------
+    // function tambah(a,b){
+    //     return a + b;
+    // }
+
+    // function kali(a,b){
+    //     return a * b;
+    // }
+
+    // var hasil = kali(tambah(1,2), tambah(3,4));
+    // console.log(hasil);
+
+// contoh arguments -------------------------------------------------
+    function tambah(){
+        var hasil = 0;
+        for(var i = 0; i < arguments.length; i++){
+            hasil += arguments[i];
+        }
+        return hasil
+    }
+
+    var coba = tambah(1,2,3,4);
+    console.log(coba);
