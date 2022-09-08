3f89ca421c0f9808c7a37264e5aef7948cec2056
fariez11 <fariezilham03@gmail.com>
2022-09-08 10:07:37 +0700

    belajar function declaration & expression + catatan

---
+++ materi33.js
@@ -0,0 +1,15 @@
+// function declaration -------------------------------------------------------
+    // function identifier (ParameterList opt) { FunctionBody}         // Rumus
+    
+    // contoh
+        function tampilPesan(nama){
+            alert('halo' + nama);
+        }
+
+// function expression --------------------------------------------------------
+    // function identifier opt (ParameterList opt) { FunctionBOdy}     // Rumus
+
+    // contoh 
+        var tampilPesan = function (nama){
+            alert('halo' + nama);
+        }
