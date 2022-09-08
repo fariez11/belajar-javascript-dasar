9164618dfb4b7a84334c29f164a55f5fcfc1986f
fariez11 <fariezilham03@gmail.com>
2022-09-08 14:17:58 +0700

    belajar manipulasi array + catatan

--- materi34-35.js
+++ materi34-35.js
@@ -23,8 +23,31 @@
     // ---------------------------
 
     var arr = [];
-    arr[0] = "Fariez";
-    arr[1] = "Ilham";
-    arr[2] = "Sandyas";
-    arr[6] = "Farah";
+    // arr[0] = "Fariez";
+    // arr[1] = "Ilham";
+    // arr[2] = "Sandyas";
+    // arr[6] = "Farah";
+    // console.log(arr);
+
+// 2. Menghapus isi array 
+    arr = ['Fariez', 'Ilham', 'Alvian', 'Syach'];
+    arr[1] = undefined;
     console.log(arr);
+
+// 3. Menampilkan isi array 
+    for(i = 0; i < arr.length; i++){
+        console.log('mahasiswa ke-' + (i+1) + ' adalah ' + arr[i]);
+    }
+
+// Method pada array ----------------------------------------------
+// 1.join
+    var arr1  = ['Fariez', 'Ilham', 'Alvian'];
+    console.log(arr1.join(' - '));
+// 2.push & pop
+    // arr1.push('Syach');                     // menambah 1 atau lebih di akhir
+    arr1.pop();                                // menghapus 1 per 1  di akhir
+    console.log(arr1.join(' - '));
+// 3.unshift & shift
+    arr.unshift('Fariez','Adnan');             // menambah 1 atau lebih di awal
+    arr1.shif();                               // menghapus 1 per 1  di awal  
+    console.log(arr.join(' - '));
