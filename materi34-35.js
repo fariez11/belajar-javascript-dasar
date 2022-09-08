1b29429993a4a0b59bf1db1008ce9259e55e14b9
fariez11 <fariezilham03@gmail.com>
2022-09-08 10:56:45 +0700

    array

--- materi34-35.js
+++ materi34-35.js
@@ -1,21 +1,30 @@
 // array -------------------------------------------------------------------------------------------------
-    var binatang = ['Kucing', 'Kelinci','Monyet','Panda','Koala','Sapi'];
-    // atau
-    var binatang = [];
-        binatang = ['Kucing', 'Kelinci','Monyet','Panda','Koala','Sapi'];
+    // var binatang = ['Kucing', 'Kelinci','Monyet','Panda','Koala','Sapi'];
+    // // atau
+    // var binatang = [];
+    //     binatang = ['Kucing', 'Kelinci','Monyet','Panda','Koala','Sapi'];
     
-    console.log(binatang[4]);
-    console.log(typeof(binatang));                              // maka akan selalu keluar object
-    console.log(binatang.length);
+    // console.log(binatang[4]);
+    // console.log(typeof(binatang));                              // maka akan selalu keluar object
+    // console.log(binatang.length);
 
-    var myArr = ['teks',2,false];
-    var myFunc = function(){
-        alert('hello world');
-    }
-    var myArr2 = ['teks',2,false,myFunc];
-    var myArr3 = ['teks',2,false,myFunc, [3,4,5]];              // array multidimensi / array bersarang
-        console.log(myArr3[4][1]);                              // untuk memanggil angka 4 yang di line 16
+    // var myArr = ['teks',2,false];
+    // var myFunc = function(){
+    //     alert('hello world');
+    // }
+    // var myArr2 = ['teks',2,false,myFunc];
+    // var myArr3 = ['teks',2,false,myFunc, [3,4,5]];              // array multidimensi / array bersarang
+    //     console.log(myArr3[4][1]);                              // untuk memanggil angka 4 yang di line 16
     
 // manipulasi array --------------------------------------------------------------------------------------
 // 1. Menambah isi array
-    var arr = 
+    // var arr =  ["a",1,true];
+    // console.log(arr); 
+    // ---------------------------
+
+    var arr = [];
+    arr[0] = "Fariez";
+    arr[1] = "Ilham";
+    arr[2] = "Sandyas";
+    arr[6] = "Farah";
+    console.log(arr);
