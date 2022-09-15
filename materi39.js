2966a0318169e2155572f678e132829a7ad896ad
fariez11 <fariezilham03@gmail.com>
2022-09-15 09:38:46 +0700

    materi39 (latihan function array) + catatan

---
+++ materi39.js
@@ -0,0 +1,7 @@
+var penumpang = [];
+var tambahPenumpang = function(namaPenumpang, penumpang){
+    // jika angkot kosong 
+        // tambah penumpang di awal array
+        // kembali isi array & keluar dari function
+}
+ 
